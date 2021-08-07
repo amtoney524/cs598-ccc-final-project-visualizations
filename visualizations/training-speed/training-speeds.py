4d1be2e34b6513327d2c4e042a9deb5422fd1122
Ashley Toney <amtoney524@gmail.com>
2021-08-07 17:16:37 -0500

    Removes title from chart

--- visualizations/training-speed/training-speeds.py
+++ visualizations/training-speed/training-speeds.py
@@ -21,10 +21,6 @@ df_hvd = df[(df['FRAMEWORK'] == 'Horovod')]
 ddp = df_ddp['TRAINING SPEED'].to_numpy()
 hvd = df_hvd['TRAINING SPEED'].to_numpy()
 
-print(labels)
-print(ddp)
-print(hvd)
-
 x = np.arange(len(labels))  # the label locations
 width = 0.35  # the width of the bars
 
@@ -35,7 +31,7 @@ rects2 = ax.bar(x + width/2, hvd, width, label='Horovod')
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('Time (seconds)')
 ax.set_xlabel('AWS EC2 Configuration')
-ax.set_title('Model Training Speed')
+#ax.set_title('Model Training Speed')
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.legend()
